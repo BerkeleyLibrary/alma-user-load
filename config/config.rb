dba6352d3ccfa48c111cdb75e23539315510dc01
Steve Sullivan <steve.sullivan@berkeley.edu>
2022-03-01 12:33:45 -0800

    add sis fields and re-add settings fetch

--- config/config.rb
+++ config/config.rb
@@ -14,13 +14,16 @@ class Config
 
   ucpath_contents = File.open('config/ucpath_fields.yml').read
   @ucpath_fields = YAML.load(ERB.new(ucpath_contents).result)
-
+  
   ldap_contents = File.open('config/ldap_fields.yml').read
   @ldap_fields = YAML.load(ERB.new(ldap_contents).result)
-
+  
   ucpath_codes = File.open('config/ucpath_codes.yml').read
   @ucpath_codes = YAML.load(ERB.new(ucpath_codes).result)
-
+  
+  sis_contents = File.open('config/sis_fields.yml').read
+  @sis_fields = YAML.load(ERB.new(sis_contents).result)
+  
   # TODO:  Add a check to make sure all necessary config settings are set!
   #        warn if we don't have an .env with necessary settings!
   #        Maybe if missing needed .env locally I can offer a command line option
@@ -38,6 +41,10 @@ class Config
 
   def self.ucpath_employee_fields
     @ucpath_fields['Employee']['fields']
+  end
+
+  def self.sis_fields
+    @sis_fields['SIS']['fields']
   end
 
   def self.ucpath_job_fields
@@ -60,9 +67,10 @@ class Config
     @ucpath_codes[type].include? value
   end
 
-  # def self.setting(field)
-  #   @settings.Settings[field] || nil
-  # end
+  # Returns specified field value from settings.yml
+  def self.setting(field)
+    @settings.Settings[field] || nil
+  end
 
   def self.help
     @settings.Help
