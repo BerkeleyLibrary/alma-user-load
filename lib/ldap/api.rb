0bbb515ba04c58e79641b5e4acacd41289c9f563
Steve Sullivan <steve.sullivan@berkeley.edu>
2022-03-01 16:21:39 -0800

    code cleanup

--- lib/ldap/api.rb
+++ lib/ldap/api.rb
@@ -1,50 +1,45 @@
+# frozen_string_literal: true
+
 require 'ostruct'
 require 'net/ldap'
 
 module LDAP
+  # Fetches an LDAP record by UID returns an ostruct
   module API
-    # def list_fields
-    #   puts "---------- LDAP::API list_fields ------------"
-    #   puts "Config.ldap_fields.inspect : #{Config.ldap_fields.inspect}"
-    #   puts "--------------------------------------"
-    # end
-
+    # rubocop:disable Metrics/MethodLength
     def fetch_ldap_rec(id)
       ldap_rec = OpenStruct.new
-
       filter = Net::LDAP::Filter.eq('uid', id)
-
-      ldap = Net::LDAP.new host: host,
-                           port: 389,
-                           auth: {
-                             method: :simple,
-                             username: 'uid=library-hrms-epl,ou=applications,dc=berkeley,dc=edu',
-                             password: pass
-                           }
+      ldap = ldap_connection
 
       # Extract required fields and bundle into open struct
       if ldap.bind
         ldap.search(base: base, filter: filter) do |entry|
           entry.each do |attribute, values|
             ldap_rec[attribute] = values
-            # values.each do |value|
-            # end
           end
         end
       end
 
       ldap_rec
     end
+    # rubocop:enable Metrics/MethodLength
 
     private
+
+    def ldap_connection
+      Net::LDAP.new host: host,
+                    port: 389,
+                    auth: {
+                      method: :simple,
+                      username: 'uid=library-hrms-epl,ou=applications,dc=berkeley,dc=edu',
+                      password: pass
+                    }
+    end
 
     def host
       Config.secrets.ldap.host
     end
-
-    # def bind
-    #   'uid=library-hrms-epl,ou=applications,dc=berkeley,dc=edu'
-    # end
 
     def base
       'ou=people,dc=berkeley,dc=edu'
