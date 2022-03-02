0416bbae2cc6435581ad47479f4c6de9ca16f408
Steve Sullivan <steve.sullivan@berkeley.edu>
2022-03-01 17:21:41 -0800

    add spec for change log days

--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 describe Config do
   it 'help returns help' do
     expect(Config.help).to start_with('Alma User Load Usage:')
+  end
+
+  it 'config setting for change log days is 14' do
+    expect(Config.setting('change_log_days')).to eq(14)
   end
 end
