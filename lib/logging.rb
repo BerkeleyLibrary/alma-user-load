0a42737a1109cb293b64f6932272d204b09b7a8f
Steve Sullivan <steve.sullivan@berkeley.edu>
2022-03-29 15:23:31 -0700

    log to stdout instead of local file

--- lib/logging.rb
+++ lib/logging.rb
@@ -10,18 +10,8 @@ require 'logger'
 module Logging
   class << self
     def logger
-      # @logger ||= Logger.new($stdout)
-      # TODO - put filename/daily in config
-      @logger ||= Logger.new('log/log.txt', 'daily')
+      @logger ||= Logger.new($stdout)
     end
-
-    # Example cribbed from stackoverflow referenced above had the following
-    # 3 lines of code. RSPEC says that they're not being hit... so wondering
-    # if they're not needed... Commenting out for now but leaving here until
-    # I'm sure they're not needed.
-    # def logger=(logger)
-    #   @logger = logger
-    # end
   end
 
   def self.included(base)
