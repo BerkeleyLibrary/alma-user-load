145b4429276085d4cb281f9e9d01545315cb913c
Steve Sullivan <steve.sullivan@berkeley.edu>
2022-03-31 17:12:11 -0700

    rubofix found a new exciting way to write to a file...yay.

--- spec/lib/secrets_spec.rb
+++ spec/lib/secrets_spec.rb
@@ -26,7 +26,7 @@ describe Docker::Secret do
         secret_name = "secret_#{prefix}_#{i}"
         secret = Base64.strict_encode64(random.bytes(128))
         expected_secrets[secret_name] = secret
-        File.open(tmpdir_path.join(secret_name), 'wb') { |f| f.write(secret) }
+        File.binwrite(tmpdir_path.join(secret_name), secret)
       end
     end
 
