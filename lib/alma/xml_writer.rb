cf65c3f7e0cabe6921936401502cf29e72e904ce
maría a. matienzo <73732+anarchivist@users.noreply.github.com>
2026-01-08 09:02:53 -0800

    AP-496: update build variables across the board (#2)
    * AP-496: update build variables across the board
    
    * version bump before release

--- lib/alma/xml_writer.rb
+++ lib/alma/xml_writer.rb
@@ -42,7 +42,7 @@ module Alma
 
     def prolog_and_opening_tag
       tag = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-      tag += "<!-- GIT_COMMIT:#{ENV['GIT_COMMIT']} -->\n"
+      tag += "<!-- GIT_SHA:#{ENV['GIT_SHA']} -->\n"
       tag += "<!-- DOCKER_TAG:#{ENV['DOCKER_TAG']} -->\n"
       tag += "<!-- VERSION: #{ENV['VERSION']} -->\n"
       tag += "<!-- CHANGE_LOG_COUNT: #{change_log_count} -->\n"
