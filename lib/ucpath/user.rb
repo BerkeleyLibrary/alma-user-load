83ad8b6d6bdbff20d01fa79788c7e737cd7139aa
Steve Sullivan <113538232+steve-sullivan@users.noreply.github.com>
2026-03-10 13:08:32 -0700

    Skip employees when percentageOfFullTime is zero (#7)
    * Skip employees when percentageOfFullTime is zero
    
    * Update version number for XML comments
    
    * Tweak full time check

--- lib/ucpath/user.rb
+++ lib/ucpath/user.rb
@@ -143,6 +143,15 @@ module UCPath
 
       job_code = jobs.job.job_code || nil
       priority_job = Config.check_ucpath_code('priority_job_codes', job_code)
+      percent_of_fulltime = jobs.job.percent_of_fulltime.to_f
+
+      # PERCENT OF FULL TIME CHECK
+      # AP-559 If an employee is in a position that is 0 FTE,
+      # their record should should be filtered out from the UCPath files.
+      if percent_of_fulltime.zero?
+        logger.info "#{id} - Ineligible: Percentage of Full Time Check: #{percent_of_fulltime}"
+        return nil
+      end
 
       # STUDENT CHECK - berkeleyeduaffiliations in Student Affiliation (ldap_fields.yml)
       if !priority_job && ldap&.berkeleyeduaffiliations
